81b4576ed30c869075f318f77d9af936cb4ec4b9
nusubbotin <nu.subbotin@ya.ru>
2022-11-20 17:13:59 +0300

    Инициализация проекта

---
+++ src/main/java/com/example/employe/EmployeeMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.employe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeMockitoApplication.class, args);
+    }
+
+}
